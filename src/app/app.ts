979c11ab45862992df201661a3ed4efa12f84c9b
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-04-20 15:17:23 +0200

    signals in input

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header';
 import { RouterOutlet } from '@angular/router';
 import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,6 @@ import { User } from "./user/user";
   styleUrl: './app.scss'
 })
 export class App {
+  users = DUMMY_USERS;
   protected readonly title = signal('first-angular-app');
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.scss',
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
