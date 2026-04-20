5fa834f5fd624e174c4320c06d238b6147eedca5
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-04-20 14:09:18 +0200

    start met header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { HeaderComponent } from './header';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/header.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+
+export class HeaderComponent {}
